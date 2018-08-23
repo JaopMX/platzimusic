023940586f215cffc9123f047a83a6e71acdce28
Tocho <tocho@Tocho-PC>
2018-08-23 13:26:38 -0500

    Inittial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+    appname: 'platzimusic',
+    apiKey: 'd2f79b8f4a08f9d2dc2f23b8fcd1d3af',
+    secret: '506046ab12b66ef7840eb94ec3d6fc4e',
+    registeredTo: 'AntonioETI'
+}
+
+// Application name	platzimusic
+// API key	d2f79b8f4a08f9d2dc2f23b8fcd1d3af
+// Shared secret	506046ab12b66ef7840eb94ec3d6fc4e
+// Registered to	AntonioETI
